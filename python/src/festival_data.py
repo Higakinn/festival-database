54f7bcdf549156f4de45cef809ebcb3ca6bc3968
hgaiji <yuukihigaki@MacBook-Pro.local>
2024-07-20 14:06:25 +0900

    update: python script upate

--- python/src/festival_data.py
+++ python/src/festival_data.py
@@ -1,5 +1,6 @@
 import requests
 import textwrap
+import datetime
 
 
 def get_all(api_token, database_id, limit=100):
@@ -11,7 +12,7 @@ def get_all(api_token, database_id, limit=100):
             # {
             #     "property": "region"
             # },
-            {"property": "is_post", "checkbox": {"equals": False}}
+            {"property": "is_post", "checkbox": {"equals": True}}
         ]
     }
     payload = {
@@ -45,7 +46,7 @@ def get_all(api_token, database_id, limit=100):
             date = _start_date
 
         url = _props.get("link").get("url")
-
+        print(r)
         result.append(
             {
                 "page_id": page_id,
@@ -59,7 +60,62 @@ def get_all(api_token, database_id, limit=100):
     return result
 
 
-def post_data(client, region, access,festival_name, date, url):
+def held_today(api_token, database_id, limit=100):
+    # notion get
+    url = f"https://api.notion.com/v1/databases/{database_id}/query"
+    # is_post=false つまり  まだ投稿していない祭礼情報を取得するためのフィルタ
+    db_filter = {
+        "and": [
+            # {
+            #     "property": "region"
+            # },
+            {"property": "is_post", "checkbox": {"equals": True}},
+            {"property": "is_repost", "checkbox": {"equals": False}},
+            {"property": "date", "date": {"equals": f"{datetime.date.today()}"}},
+            {"property": "date", "date": {"this_week": {}}},
+        ]
+    }
+    payload = {
+        "filter": db_filter,
+        "page_size": limit,
+    }
+    headers = {
+        "Accept": "application/json",
+        "Notion-Version": "2022-06-28",
+        "Content-Type": "application/json",
+        "Authorization": f"{api_token}",
+    }
+
+    response = requests.post(url, json=payload, headers=headers).json()
+    result = []
+    for r in response.get("results"):
+        page_id = r.get("id")
+
+        _props = r.get("properties")
+        festival_name = _props.get("festival_name").get("title")[0].get("plain_text")
+        region = _props.get("region").get("rich_text")[0].get("plain_text")
+        access = _props.get("access").get("rich_text")[0].get("plain_text")
+        x_url = _props.get("x url").get("formula").get("string")
+        _date_dict = _props.get("date").get("date")
+        start_date = _date_dict.get("start")
+        end_date = _date_dict.get("end")
+
+        print(x_url)
+        result.append(
+            {
+                "page_id": page_id,
+                "festival_name": festival_name,
+                "region": region,
+                "start_date": start_date,
+                "end_date": end_date,
+                "access": access,
+                "x_url": x_url,
+            }
+        )
+    return result
+
+
+def post_data(client, region, access, festival_name, date, url):
     post_content = (
         textwrap.dedent(
             """
@@ -91,11 +147,65 @@ def post_data(client, region, access,festival_name, date, url):
     return {"post_id": _post_result.data.get("id")}
 
 
+def _repost_content(region, access, festival_name, start_date, end_date, url):
+    today = datetime.date.today()
+    repost_content = (
+        textwrap.dedent(
+            """
+【{region}】
+#{festival_name} 始まります！
+
+■ アクセス
+・{access}
+
+{post_url}
+  """
+        )
+        .format(
+            region=region,
+            access=access,
+            festival_name=festival_name,
+            post_url=url,
+        )
+        .strip()
+    )
+    return repost_content
+
+
+def repost(client, region, access, festival_name, start_date, end_date, url):
+    content = _repost_content(region, access, festival_name, start_date, end_date, url)
+    _repost_result = client.create_tweet(text=content)
+    return {"repost_id": _repost_result.data.get("id")}
+
+
 def update(api_token, page_id, post_id):
     url = f"https://api.notion.com/v1/pages/{page_id}"
     update_props = {
         "is_post": {"checkbox": True},
         "post_id": {"rich_text": [{"text": {"content": post_id}}]},
+    }
+    payload = {
+        "properties": update_props,
+    }
+    headers = {
+        "Accept": "application/json",
+        "Notion-Version": "2022-06-28",
+        "Content-Type": "application/json",
+        "Authorization": f"{api_token}",
+    }
+
+    response = requests.patch(url, json=payload, headers=headers)
+    if response.status_code == requests.codes.ok:
+        return {"ok": True}
+
+    return {"ok": False}
+
+
+def update_repost(api_token, page_id, repost_id):
+    url = f"https://api.notion.com/v1/pages/{page_id}"
+    update_props = {
+        "is_repost": {"checkbox": True},
+        "repost_id": {"rich_text": [{"text": {"content": repost_id}}]},
     }
     payload = {
         "properties": update_props,
--- python/src/main.py
+++ python/src/main.py
@@ -4,25 +4,26 @@ import os
 
 
 def main():
-    print("python batch start!!")
+    # 環境変数を取得
     NOTION_API_TOKEN = os.getenv("NOTION_API_TOKEN")
     NOTION_DATABASE_ID = os.getenv("NOTION_DATABASE_ID")
-    festivals = festival_data.get_all(NOTION_API_TOKEN, NOTION_DATABASE_ID)
-
     X_API_KEY = os.getenv("X_API_KEY")
     X_API_KEY_SECRET = os.getenv("X_API_KEY_SECRET")
     X_API_BEARER_TOKEN = os.getenv("X_API_BEARER_TOKEN")
     X_API_ACCESS_TOKEN = os.getenv("X_API_ACCESS_TOKEN")
     X_API_ACCESS_TOKEN_SECRET = os.getenv("X_API_ACCESS_TOKEN_SECRET")
-
     # 認証
-    client = tweepy.Client(
+    x_client = tweepy.Client(
         bearer_token=X_API_BEARER_TOKEN,
         consumer_key=X_API_KEY,
         consumer_secret=X_API_KEY_SECRET,
         access_token=X_API_ACCESS_TOKEN,
         access_token_secret=X_API_ACCESS_TOKEN_SECRET,
     )
+
+    print("python batch start!!")
+    print("festival data post")
+    festivals = festival_data.get_all(NOTION_API_TOKEN, NOTION_DATABASE_ID)
     for festival in festivals:
         region = festival.get("region")
         access = festival.get("access")
@@ -31,8 +32,8 @@ def main():
         url = festival.get("url")
         page_id = festival.get("page_id")
         print(festival_name)
-        post_id = festival_data.post_data(
-            client=client,
+        post_id = festival_data.post(
+            client=x_client,
             region=region,
             access=access,
             festival_name=festival_name,
@@ -41,6 +42,27 @@ def main():
         ).get("post_id")
         print(post_id)
         festival_data.update(NOTION_API_TOKEN, page_id, post_id)
+
+    print("festival data repost")
+    festivals = festival_data.held_today(NOTION_API_TOKEN, NOTION_DATABASE_ID)
+    for festival in festivals:
+        region = festival.get("region")
+        access = festival.get("access")
+        festival_name = festival.get("festival_name")
+        start_date = festival.get("start_date")
+        end_date = festival.get("end_date")
+        page_id = festival.get("page_id")
+        x_url = festival.get("x_url")
+        repost_id = festival_data.repost(
+            client=x_client,
+            region=region,
+            access=access,
+            festival_name=festival_name,
+            start_date=start_date,
+            end_date=end_date,
+            url=x_url,
+        ).get("repost_id")
+        festival_data.update_repost(NOTION_API_TOKEN, page_id, repost_id)
 
     print("python batch end!!")
 
